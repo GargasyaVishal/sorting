9e95f388c755108354a098532c556d44b45326b3
vishal.gargasya <vishal.gargasya@CSELAB02-08.bennett.edu.in>
2017-08-22 12:19:09 +0530

    NewSort2

--- sorting/src/sorting/bubblesort.java
+++ sorting/src/sorting/bubblesort.java
@@ -1,5 +1,51 @@
 package sorting;
 
+
 public class bubblesort {
+	
+	static void bubblesort(int[]arr){
+	
+	
+	int n=arr.length;
+	 int temp = 0;
+	for(int i=0;i<n;i++){
+		for(int j=0;j<(n-1);j++){
+			if(arr[j-1]>arr[j]){
+				//swap array
+				temp=arr[j-1];
+				arr[j-1]=arr[j];
+				arr[j]=temp;
+				
+			}
+		}
+		
+		
+	}
 
 }
+
+public static void main (String args[]){
+	int arr[]={9,2,4,3,6,5,7,8,0};
+	
+	   System.out.println("Array Before Bubble Sort");  
+       for(int i=0; i < arr.length; i++){  
+               System.out.print(arr[i] + " ");  
+       }  
+       System.out.println();  
+         
+       bubblesort(arr);
+       //sorting array elements using bubble sort  
+        
+       System.out.println("Array After Bubble Sort");  
+       for(int i=0; i < arr.length; i++){  
+               System.out.print(arr[i] + " ");  
+       }  
+
+}  
+	
+}
+
+
+
+
+
